101413fe58f4f58d7266396d1dfcc3e13bf052bd
Hycarus <mirkomarasco08@gmail.com>
2023-11-15 12:11:04 +0100

    created scaffholding

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- 04b63a97f719edbe743291502d732b6d  API KEY -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
