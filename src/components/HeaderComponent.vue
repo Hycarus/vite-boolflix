4ece1020f860080c311ebb642d40ea106572104a
Hycarus <mirkomarasco08@gmail.com>
2023-11-18 12:19:33 +0100

    changed header style

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,60 +1,52 @@
 <template>
-    <header class="d-flex justify-content-between align-items-center">
-        <div class="d-flex align-items-center ">
-            <img src="/images/netflix-logo.png" alt="logo">
+    <header class="d-flex justify-content-between align-items-center" @click.self="store.isActive = false">
+        <div>
             <nav class="navbar navbar-expand-lg">
-                <div class="container-fluid">
-                    <div class="collapse navbar-collapse" id="">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active text-light" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="#">Serie TV</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="#">Film</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="#">Nuovi e popolari</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="#">La mia lista</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="#">Sfoglia per lingua</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 align-items-center ">
+                    <li class="nav-item">
+                        <img src="/images/netflix-logo.png" alt="logo">
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Serie TV</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Film</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Nuovi e popolari</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">La mia lista</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Sfoglia per lingua</a>
+                    </li>
+                    <li class="nav-item">
+                        <select name="genres" id="genres" v-model="store.genresSearch" @change="$emit('genresChange')">
+                            <option value="">All</option>
+                            <option v-for="genres in store.genresList" :key="genres" :value="genres.id">
+                                {{ genres.name }}
+                            </option>
+                        </select>
+                    </li>
+                </ul>
             </nav>
         </div>
-        <div>
-            <select name="genres" id="genres" v-model="store.genresSearch" @change="$emit('genresChange')">
-                <option value="">All</option>
-                <option v-for="genres in store.genresList" :key="genres" :value="genres.id">
-                {{ genres.name }}
-                </option>
-            </select>
-        </div>
-        <div class="d-flex justify-content-evenly align-items-center">
-            <div class="input-group w-50 flex-nowrap">
-                <input class="form-control" placeholder="Inserisci il film/serie tv che vuoi cercare" type="text" v-model="store.params.query" @keyup.enter="$emit('search', textSearch), store.showMain = true, scrollPage()">
-                <button class="btn btn-success" @click="$emit('search', textSearch), store.showMain = true, scrollPage()">Search</button>
+        <div class="d-flex justify-content-center align-items-center">
+            <i ref="icon" @click.stop="(store.isActive = !store.isActive)" :class="( store.isActive ? 'd-none' : 'd-inline-block')" class="fa-solid fa-magnifying-glass fs-5 text-light"></i>
+            <input class="form-control" :class="(store.isActive ? 'd-inline-block bigger' : 'd-none')" placeholder="Inserisci il film/serie tv che vuoi cercare" type="text" v-model="store.params.query" @keyup.enter="$emit('search', textSearch), store.showMain = true, scrollPage(), store.isActive = !store.isActive">
+            <button class="btn btn-danger mx-2" @click="$emit('search', textSearch), store.showMain = true, scrollPage()">Search</button>
+            <div class="text-light text-center me-3">
+                {{ store.profileList[store.profileIndex].name }}
             </div>
-            <div class="user d-flex align-items-center">
-                <div class="text-light text-center me-3">
-                    {{ store.profileList[store.profileIndex].name }}
-                </div>
-                <div>
-                    <img :src="store.profileList[store.profileIndex].profilePic" :alt="store.profileList[store.profileIndex].descriptionPic">
-                </div>
-                
+            <div>
+                <img :src="store.profileList[store.profileIndex].profilePic" :alt="store.profileList[store.profileIndex].descriptionPic">
             </div>
         </div>
-        
     </header>
-    
 </template>
 
 <script>
@@ -86,9 +78,35 @@ img{
     width: 70px;
     height: 70px;
 }
+ul{
+    li:first-child{
+        margin-bottom: 10px;
+    }
+}
 .user{
     width: 100px;
     height: 100px;
 }
+input{
+    width: 250px;
+    height: 30px;
+    padding-bottom: 10px;
+    background-color: black;
+    border: 1px solid white;
+    color: white;
 
+    &::placeholder{
+        color: grey !important;
+        text-align: center;
+        line-height: 30px;
+    }
+}
+.bigger{
+        animation-name: bigger;
+        animation-duration: 0.4s;
+    }
+@keyframes bigger{
+        0% {width: 0px;}
+        100% {width: 250px;}
+    }
 </style>
